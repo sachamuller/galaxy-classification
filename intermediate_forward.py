1da6c692a6f10d35b89909a97958008fe8c08dbd
Sacha Muller <chloe.muller@student-cs.fr>
2023-03-04 11:28:46 +0100

    added model name to saved activation maps h5 name

--- intermediate_forward.py
+++ intermediate_forward.py
@@ -20,7 +20,7 @@ def get_intermediate_dataset(
 ):
     activation_maps_path = os.path.join(
         config["paths"]["intermediate_dataset_folder"],
-        f'{config["paths"]["intermediate_dataset_name"]}_{config["last_frozen_layer"]}.h5',
+        f'{config["paths"]["intermediate_dataset_name"]}_{config["model"]}_{config["last_frozen_layer"]}.h5',
     )
 
     if os.path.exists(activation_maps_path):
